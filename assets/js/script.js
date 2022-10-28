df5ad532873f10fed990cd3a35395340e43be131
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-28 16:03:56 +0200

    milestone 3 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,11 @@ createApp({
 
             messageStatus: false,
 
-            message: "Congratulazioni! hai completato le tue tasks"
+            message: "Congratulazioni! hai completato le tue tasks",
+
+            text: "",
+
+            inputStatus: true
         }
     },
 
@@ -43,6 +47,25 @@ createApp({
             if(this.tasks.length === 0) {
 
                 this.messageStatus = true
+            }
+
+        },
+
+        addTask () {
+
+            if (this.text.length < 5) {
+
+                this.inputStatus = false
+
+                this.text = ""
+            } else {
+
+                this.tasks.push({
+                    text: this.text,
+                    done: false
+                }) 
+                
+                this.text = ""
             }
 
         }
